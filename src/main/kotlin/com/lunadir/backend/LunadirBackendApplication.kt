1dcf5588a1431d6b83ae4e4d3f262c5d060723b2
2uent1n <127933396+2uent1n@users.noreply.github.com>
2025-08-17 17:44:02 +0200

    Initial commit

---
+++ src/main/kotlin/com/lunadir/backend/LunadirBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.lunadir.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LunadirBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<LunadirBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/lunadir/backend/LunadirBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lunadir.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LunadirBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
